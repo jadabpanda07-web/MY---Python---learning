2f675e21ab8434b644464a8ccdecf7da03fb0113
jadabpanda07-web <jadabpanda07-web@users.noreply.github.com>
2026-08-25 20:18:09 +0530

    finding maximum and minimum

--- question_21.py
+++ question_21.py
@@ -1,10 +1,23 @@
 # Find the largest and smallest element in a list without using max() and min()
-list = []
+numbers = []
 
-n = int(input('Enter a number: '))
+n = int(input('Enter the number of elements: '))
 
 for i in range(n):
-    
-    element = input('Enter element in list: ')
-    list.append(element)
-print(list)
+    element = float(input('Enter an element: '))
+    numbers.append(element)
+
+if not numbers:
+    print('The list is empty.')
+else:
+    smallest = numbers[0]
+    largest = numbers[0]
+
+    for number in numbers[1:]:
+        if number < smallest:
+            smallest = number
+        if number > largest:
+            largest = number
+
+    print('Smallest element:', smallest)
+    print('Largest element:', largest)
