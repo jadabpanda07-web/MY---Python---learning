9b1f3c5954652caba499891cbb5c01ec58e41a78
jadabpanda07-web <jadabpanda07-web@users.noreply.github.com>
2026-08-25 19:42:43 +0530

    Add Python learning exercise

---
+++ question_21.py
@@ -0,0 +1,10 @@
+# Find the largest and smallest element in a list without using max() and min()
+list = []
+
+n = int(input('Enter a number: '))
+
+for i in range(n):
+    
+    element = input('Enter element in list: ')
+    list.append(element)
+print(list)
